d00db5f60ea5bdee9f47c8c88311b44ba7a4cb9b
cindy <expectative06@gmail.com>
2021-03-23 17:37:36 +0800

    Add project

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by  cindy on 2021/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
